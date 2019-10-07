4811c3fff1a767ba446c5b8fa9ad5c73dbf1ed8d
nate mccay <nate.mccay@ymail.com>
2019-10-07 14:13:43 -0500

    monday

--- BestBuyCRUD/BestBuyCRUD/Program.cs
+++ BestBuyCRUD/BestBuyCRUD/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
 namespace BestBuyCRUD
 {
@@ -6,7 +8,30 @@ namespace BestBuyCRUD
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var departments = GetAllDepartments();
+            foreach(var department in departments)
+            {
+                Console.WriteLine(department);
+            }
+        }
+        static List<string> GetAllDepartments()
+        {
+            MySqlConnection conn = new MySqlConnection();
+            conn.ConnectionString = System.IO.File.ReadAllText("connectionString.txt");
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT Name FROM Departments;";
+            using (conn)
+            {
+                conn.Open();
+                List<string> allDepartments = new List<string>();
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while(reader.Read() == true)
+                {
+                    var currentDepartment = reader.GetString("Name");
+                    allDepartments.Add(currentDepartment);
+                }
+                return allDepartments;
+            }
         }
     }
 }
